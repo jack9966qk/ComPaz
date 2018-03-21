9a06b19fec9b862e6a64901d471ff71caa1be402
Jack <jack9966qk@gmail.com>
2018-03-21 21:40:08 +1100

    printer variable declaration part

--- stage1_framework/PazPrinter.hs
+++ stage1_framework/PazPrinter.hs
@@ -5,63 +5,150 @@ import PazParser (
     ASTVariableDeclarationPart,
     ASTProcedureDeclarationPart,
     ASTCompoundStatement,
-    ASTVariableDeclaration
+    ASTVariableDeclaration,
+    ASTIdentifierList,
+    ASTTypeDenoter,
+    TypeDenoter(..),
+    ASTTypeIdentifier,
+    TypeIdentifier(..),
+    ASTArrayType,
+    ASTSubrangeType,
+    ASTConstant,
+    ASTSign,
+    Sign(..)
     )
 
 import PazLexer (
-    ASTIdentifier
+    ASTIdentifier,
+    ASTUnsignedInteger
     )
 
 type IndentationLvl = Int
 type PprintObj a = (IndentationLvl, a)
 
-pprintSepBy :: IO () -> [IO ()] -> IO ()
-pprintSepBy _ [] = return ()
-pprintSepBy _ [x] = x
-pprintSepBy sep (x:y:zs) =
-    x >> sep >> (pprintSepBy sep (y:zs))
+printSepBy :: IO () -> [IO ()] -> IO ()
+printSepBy _ [] = return ()
+printSepBy _ [x] = x
+printSepBy sep (x:y:zs) =
+    x >> sep >> (printSepBy sep (y:zs))
 
-pprintLineBreakWithIndentation :: Int -> IO ()
-pprintLineBreakWithIndentation lvl = (putStr "\n") >> (pprintIndentation lvl)
+printIndentation :: Int -> IO ()
+printIndentation 0 = return ()
+printIndentation n = (putStr "    ") >> (printIndentation (n-1))
 
-pprintIndentation :: Int -> IO ()
-pprintIndentation 0 = return ()
-pprintIndentation n = (putStr "    ") >> (pprintIndentation (n-1))
+pprintLineBreak :: PprintObj () -> IO ()
+pprintLineBreak (lvl, _) = (putStr "\n") >> (printIndentation lvl)
+
+replace :: PprintObj a -> b -> PprintObj b
+replace (lvl, _) x = (lvl, x)
+
+empty :: PprintObj a -> PprintObj ()
+empty (lvl, _) = (lvl, ())
+
+levelUp :: PprintObj a -> PprintObj a
+levelUp (lvl, x) = (lvl + 1, x)
+
+ast :: PprintObj a -> a
+ast (_, x) = x
+
+printSpace :: IO ()
+printSpace = putStr " "
 
 pprintProgram :: ASTProgram -> IO ()
 pprintProgram prog = pprintProgram' (0, prog)
 
 pprintProgram' :: PprintObj ASTProgram -> IO ()
-pprintProgram' (lvl, (id, var, pro, com)) = do
-    pprintTokenProgram (lvl, ())
-    putStr " "
-    pprintIdentifier (lvl, id)
-    pprintSepBy (pprintLineBreakWithIndentation lvl) [
-        pprintTokenSemicolon (lvl, ()),
-        pprintVariableDeclarationPart (lvl, var),
-        pprintProcedureDeclarationPart (lvl, pro),
-        pprintCompondStatement (lvl, com)
+pprintProgram' obj@(lvl, (id, var, pro, com)) = do
+    let e = empty obj
+    let lineBreak = pprintLineBreak e
+    pprintTokenProgram e
+    printSpace
+    pprintIdentifier $ replace obj id
+    printSepBy (lineBreak >> lineBreak) [
+        pprintTokenSemicolon e,
+        pprintVariableDeclarationPart $ replace obj var,
+        pprintProcedureDeclarationPart $ replace obj pro,
+        pprintCompondStatement $ replace obj com
         ]
-    pprintTokenDot (lvl, ())
+    pprintTokenDot e
 
-pprintTokenDot :: PprintObj () -> IO ()
-pprintTokenDot _ = putStr "."
 
-pprintTokenSemicolon :: PprintObj () -> IO ()
-pprintTokenSemicolon _ = putStr ";"
 
 pprintVariableDeclarationPart :: PprintObj ASTVariableDeclarationPart -> IO ()
 pprintVariableDeclarationPart (_, Nothing) = return ()
-pprintVariableDeclarationPart (lvl, Just (decl, moreDecl)) = do
+pprintVariableDeclarationPart obj@(lvl, Just (decl, moreDecl)) = do
+    let e = empty obj
+    let e2 = levelUp e
     let decls = decl:moreDecl
-    let pprintDecls = map (\d -> pprintVariableDeclaration (lvl+1, d)) decls
-    let ppLineBreakIndented = pprintLineBreakWithIndentation (lvl+1)
-    pprintTokenVar (lvl, ())
-    ppLineBreakIndented
-    pprintSepBy ppLineBreakIndented pprintDecls
+    let pprintDecl d = do
+        pprintVariableDeclaration $ replace e2 d
+        pprintTokenSemicolon e
+    pprintTokenVar e
+    pprintLineBreak e2
+    printSepBy (pprintLineBreak e2) (map pprintDecl decls)
 
 pprintVariableDeclaration :: PprintObj ASTVariableDeclaration -> IO ()
-pprintVariableDeclaration _ = putStr "[TODO] Variable Declaration"
+pprintVariableDeclaration obj = do
+    let (idList, typeDenoter) = ast obj
+    pprintIdentifierList $ replace obj idList
+    pprintTokenColon $ empty obj
+    printSpace
+    pprintTypeDenoter $ replace obj typeDenoter
+    
+pprintIdentifierList :: PprintObj ASTIdentifierList -> IO ()
+pprintIdentifierList obj@(_, (id, moreId)) = do
+    let idList = id:moreId
+    let pprintId x = pprintIdentifier $ replace obj x
+    printSepBy (pprintTokenComma $ empty obj) (map pprintId idList)
+
+pprintTypeDenoter :: PprintObj ASTTypeDenoter -> IO ()
+pprintTypeDenoter obj@(_, OrdinaryTypeDenoter id) =
+    pprintTypeIdentifier $ replace obj id
+
+pprintTypeDenoter obj@(_, ArrayTypeDenoter arrayType) = do
+    pprintArrayType $ replace obj arrayType
+
+pprintTypeIdentifier :: PprintObj ASTTypeIdentifier -> IO ()
+pprintTypeIdentifier obj =
+    let e = empty obj in
+        case ast obj of
+            IntegerTypeIdentifier   -> pprintTokenInteger e
+            RealTypeIdentifier      -> pprintTokenReal e
+            BooleanTypeIdentifier   -> pprintTokenBoolean e
+
+pprintArrayType :: PprintObj ASTArrayType -> IO ()
+pprintArrayType obj@(_, (subrangeType, typeId)) = do
+    let e = empty obj
+    pprintTokenArray e
+    printSpace
+    pprintTokenLeftBracket e
+    pprintSubrangeType $ replace obj subrangeType
+    pprintTokenRightBracket e
+    printSpace
+    pprintTokenOf e
+    printSpace
+    pprintTypeIdentifier $ replace obj typeId
+
+pprintSubrangeType :: PprintObj ASTSubrangeType -> IO ()
+pprintSubrangeType obj@(_, (c1, c2)) = do
+    pprintConstant $ replace obj c1
+    pprintTokenEllipsis $ empty obj
+    pprintConstant $ replace obj c2
+
+pprintConstant :: PprintObj ASTConstant -> IO ()
+pprintConstant obj@(_, (maybeSign, unsignedInt)) = do
+    case maybeSign of
+        Just sign -> pprintSign $ replace obj sign
+        Nothing   -> return ()
+    pprintUnsignedInteger $ replace obj unsignedInt
+
+pprintSign :: PprintObj ASTSign -> IO ()
+pprintSign obj@(_, sign) = case sign of
+    SignPlus    -> pprintTokenPlus $ empty obj
+    SignMinus   -> pprintTokenMinus $ empty obj
+
+pprintUnsignedInteger :: PprintObj ASTUnsignedInteger -> IO ()
+pprintUnsignedInteger (_, x) = putStr x
 
 pprintProcedureDeclarationPart :: PprintObj ASTProcedureDeclarationPart -> IO ()
 pprintProcedureDeclarationPart _ = putStr "[TODO] Procedure Declaration part"
@@ -69,9 +156,70 @@ pprintProcedureDeclarationPart _ = putStr "[TODO] Procedure Declaration part"
 pprintCompondStatement :: PprintObj ASTCompoundStatement -> IO ()
 pprintCompondStatement _ = putStr "[TODO] Compound Statement"
 
+-- Tokens
+
 pprintIdentifier :: PprintObj ASTIdentifier -> IO ()
 pprintIdentifier (_, s) = putStr s
 
+pprintTokenLeftParenthesis :: PprintObj () -> IO ()
+pprintTokenLeftParenthesis _ = putStr "("
+
+pprintTokenRightParenthesis :: PprintObj () -> IO ()
+pprintTokenRightParenthesis _ = putStr ")"
+
+pprintTokenTimes :: PprintObj () -> IO ()
+pprintTokenTimes _ = putStr "*"
+
+pprintTokenPlus :: PprintObj () -> IO ()
+pprintTokenPlus _ = putStr "+"
+
+pprintTokenComma :: PprintObj () -> IO ()
+pprintTokenComma _ = putStr ","
+
+pprintTokenMinus :: PprintObj () -> IO ()
+pprintTokenMinus _ = putStr "-"
+
+pprintTokenEllipsis :: PprintObj () -> IO ()
+pprintTokenEllipsis _ = putStr ".."
+
+pprintTokenDot :: PprintObj () -> IO ()
+pprintTokenDot _ = putStr "."
+
+pprintTokenDivideBy :: PprintObj () -> IO ()
+pprintTokenDivideBy _ = putStr "/"
+
+pprintTokenAssign :: PprintObj () -> IO ()
+pprintTokenAssign _ = putStr ":="
+
+pprintTokenColon :: PprintObj () -> IO ()
+pprintTokenColon _ = putStr ":"
+
+pprintTokenSemicolon :: PprintObj () -> IO ()
+pprintTokenSemicolon _ = putStr ";"
+
+pprintTokenLessThanOrEqual :: PprintObj () -> IO ()
+pprintTokenLessThanOrEqual _ = putStr "<="
+
+pprintTokenNotEqual :: PprintObj () -> IO ()
+pprintTokenNotEqual _ = putStr "<>"
+
+pprintTokenLessThan :: PprintObj () -> IO ()
+pprintTokenLessThan _ = putStr "<"
+
+pprintTokenEqual :: PprintObj () -> IO ()
+pprintTokenEqual _ = putStr "="
+
+pprintTokenGreaterThanOrEqual :: PprintObj () -> IO ()
+pprintTokenGreaterThanOrEqual _ = putStr ">="
+
+pprintTokenGreaterThan :: PprintObj () -> IO ()
+pprintTokenGreaterThan _ = putStr ">"
+
+pprintTokenLeftBracket :: PprintObj () -> IO ()
+pprintTokenLeftBracket _ = putStr "["
+
+pprintTokenRightBracket :: PprintObj () -> IO ()
+pprintTokenRightBracket _ = putStr "]"
 
 -- Keywords
 
