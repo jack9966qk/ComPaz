934e0a343cc3bb08a73813e21152044549c94eee
Jack <jack9966qk@gmail.com>
2018-03-30 23:30:21 +1100

    main.hs to handle command line arguments as specified

--- stage1_framework/main.hs
+++ stage1_framework/main.hs
@@ -7,30 +7,49 @@ import PazParser
 import PazPrinter
 import System.IO (hPutStrLn, stderr)
 import System.Exit (exitFailure)
+import System.Environment (getArgs)
 
 die :: String -> IO ()
 die err = do
     hPutStrLn stderr err
     exitFailure
 
-main :: IO ()
-main = do
-    text <- getContents
+fullParse :: String -> IO (Either String PazParser.ASTProgram)
+fullParse text = do
     case
         trace
-             "*** Lexical analysis"
-             (parse PazLexer.parseStartSymbol "(stdin)" text)
+            "*** Lexical analysis"
+            (parse PazLexer.parseStartSymbol "(stdin)" text)
         of
-        Left error ->
+        Left error -> do
             die ("Lexical error:\n" ++ show error)
+            return (Left "Lexical Error")
         Right tokens ->
             case
                 trace
                     "*** Syntax analysis"
                     (parse PazParser.parseStartSymbol "(stdin)" tokens)
                 of
-                Left error ->
+                Left error -> do
                     die ("Syntax error:\n" ++ show error)
-                Right ast -> do
-                    putStrLn "AST result:\n\n======================"
-                    pprintProgram ast
+                    return (Left "Syntax Error")
+                Right ast ->
+                    return (Right ast)
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        ["-f", filename] ->
+            (do
+                text <- readFile filename
+                parseResult <- fullParse text
+                case parseResult of
+                    Left _ -> return ()
+                    Right ast ->
+                        trace 
+                            "AST result:\n\n======================"
+                            (pprintProgram ast)
+            )
+        [_] -> putStrLn "Sorry, cannot generate code yet"
+        _   -> putStrLn "Usage: Paz [-p] source_file"
